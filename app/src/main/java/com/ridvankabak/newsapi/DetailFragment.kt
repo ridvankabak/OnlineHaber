30db846497347ab051bbef9a1e5f29f1d4050aeb
Rıdvan <ridvan.kbk20@gmail.com>
2020-08-07 12:35:34 +0300

    tasarim

---
+++ app/src/main/java/com/ridvankabak/newsapi/DetailFragment.kt
@@ -0,0 +1,35 @@
+package com.ridvankabak.newsapi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+
+class DetailFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val url = arguments?.let { DetailFragmentArgs.fromBundle(it).url }
+
+        println(url)
+
+        button_detail?.setOnClickListener{view ->
+            val action = DetailFragmentDirections.actionDetailFragmentToHomeFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
+    }
+}
---
+++ app/src/main/java/com/ridvankabak/newsapi/SearchFragment.kt
@@ -0,0 +1,29 @@
+package com.ridvankabak.newsapi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_search.*
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragmen
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonSearch?.setOnClickListener{view ->
+            val action = SearchFragmentDirections.actionSearchFragmentToHomeFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
+    }
+}
